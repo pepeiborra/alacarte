3f1e84bfbb712268c4b2f68d5c7b51f75b7ba126
pepe <pepe@ender4.dsic.upv.es>
2008-11-13 16:05:05 +0100

    NFData instances too
    darcs-hash:20081113150505-26539-5b3000dc690576b2caab82ee9377803d78435bdd.gz

--- Data/AlaCarte/Instances.hs
+++ Data/AlaCarte/Instances.hs
@@ -3,6 +3,7 @@
 module Data.AlaCarte.Instances where
 
 import Control.Applicative
+import Control.Parallel.Strategies
 import Data.AlaCarte
 import Test.QuickCheck
 
@@ -11,3 +12,10 @@ instance Arbitrary (f(Expr f)) => Arbitrary (Expr f) where
 
 instance (Arbitrary (f a), Arbitrary (g a)) => Arbitrary ((f :+: g) a) where
     arbitrary = oneof [Inr <$> arbitrary, Inl <$> arbitrary]
+
+
+instance NFData (f (Expr f)) => NFData (Expr f) where rnf (In x) = rnf x
+
+instance (NFData (f a), NFData (g a)) => NFData ((f :+: g) a) where
+  rnf (Inl l) = rnf l
+  rnf (Inr r) = rnf r
