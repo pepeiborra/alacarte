2daba5d6f773d197854fe4b0c87cf21cb6c3bc31
Pepe Iborra <pepeiborra@gmail.com>
2009-06-01 15:50:27 +0200

    Add a PprF instance for coproducts

--- Data/AlaCarte/Ppr.hs
+++ Data/AlaCarte/Ppr.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE TypeOperators #-}
 module Data.AlaCarte.Ppr (module Data.AlaCarte, PprF(..), pprExpr) where
 
 import Data.AlaCarte
 import Text.PrettyPrint (Doc)
 
 class Functor f => PprF f where pprF :: f Doc -> Doc
+instance (PprF f, PprF g) => PprF (f :+: g) where
+  pprF (Inl l) = pprF l; pprF (Inr r) = pprF r
 
 pprExpr :: PprF f => Expr f -> Doc
 pprExpr = foldExpr pprF
